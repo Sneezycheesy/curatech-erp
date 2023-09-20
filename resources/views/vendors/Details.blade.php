8c3f7c80f1c726221cfa6f68862d47f47aa95738
Sneezycheesy <joshii@LAPTOP-JOSH.localdomain>
2023-09-20 16:01:46 +0200

    Updated naming convention blade view files

--- resources/views/vendors/Details.blade.php
+++
@@ -1,7 +0,0 @@
-<x-app-layout>
-    <div class="max-w-7xl w-full mx-auto p-5 mt-5 bg-gray-300 dark:bg-gray-700 rounded-xl text-center">
-        <x-edit-button hx-get="{{route('vendors.edit', $vendor->id)}}" class="max-w-md mx-auto py-3 w-full">Wijzigen</x-edit-button>
-    </div>
-    
-    @include('vendors.partials.details')
-</x-app-layout>
