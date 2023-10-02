9a80d6b8d57acbcb0c784daf251b348d933d1971
Sneezycheesy <j.niksnl@outlook.com>
2023-10-02 08:44:16 +0200

    removed htmx from delete confirm prompt

--- resources/views/curatech_components/partials/vendor-delete-confirm.blade.php
+++ resources/views/curatech_components/partials/vendor-delete-confirm.blade.php
@@ -10,13 +10,13 @@
     </div>
     <div class="absolute p-3 top-1/2 left-1/2 transform -translate-y-full -translate-x-1/2 bg-cbg-300 dark:bg-cbg-900 max-w-md rounded"  @click.outside="confirm_delete_modal_open = false">
         <p class="mb-3">Weet u zeker dat u leverancier "{{$vendor->name}}" bij dit component wilt verwijderen?</p>
-        <form class="flex w-full justify-end">
+        <form class="flex w-full justify-end" wire:submit.prevent="detachVendor({{$vendor->id}})">
             @csrf
-            <x-primary-button class="mr-2" wire:click="detachVendor({{$vendor->id}})" 
-            hx-include="[name='vendor_id_{{$vendor->id}}']" hx-target="#vendor_listitem_{{$vendor->id}}" hx-swap="outerHTML">
+            <x-primary-button type="submit" class="mr-2">
                 OK
             </x-primary-button>
             <x-primary-button @click="confirm_delete_modal_open = false">X</x-primary-button>
+            </div>
         </form>
     </div>
 </div>
