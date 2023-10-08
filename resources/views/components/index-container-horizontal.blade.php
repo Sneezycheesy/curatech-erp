0e7a3c7bf22caa3570467c503e622b6fc00f2206
Sneezycheesy <j.niksnl@outlook.com>
2023-10-08 15:11:28 +0200

    Fixed htmx-indicator location

--- resources/views/components/index-container-horizontal.blade.php
+++ resources/views/components/index-container-horizontal.blade.php
@@ -1,6 +1,10 @@
-<div id="index-container-horizontal" class="relative grid overflow-x-scroll grid-rows-2 grid-flow-col auto-cols-max gap-2 w-full p-2 mx-auto scrollbar-hide">
-    {{$slot}}
-    <i id="container-horizontal-loading-indicator" class="fixed right-0 top-1/3 translate-y-1/4 htmx-indicator row-span-2 fas fa-spinner fa-spin text-5xl text-center text-black dark:text-white col-span-full"></i>
+<div class="relative">
+    <div id="index-container-horizontal" class="grid overflow-x-scroll grid-rows-2 grid-flow-col auto-cols-max gap-2 w-full p-2 mx-auto scrollbar-hide">
+        {{$slot}}
+    </div>
+    <i id="container-horizontal-loading-indicator" class="htmx-indicator absolute top-1/2 right-0 -translate-y-1/2 h-max text-5xl text-center text-black dark:text-white">
+        <i class="fas fa-spinner fa-spin"></i>
+    </i>
 </div>
 
 <script lang="text/javascript">
